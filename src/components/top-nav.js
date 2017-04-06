8afcdae14fb3a6fbfc5fcc9c6861393290d55faa
HexisHacks <franklin.a.carvajal@gmail.com>
2017-04-06 19:23:33 -0400

    Finished Hot and Cold App

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,30 +1,17 @@
 import React from 'react';
 
-export default class TopNav extends React.Component {
-    info(event) {
-        if(this.props.info) {
-            this.props.info();
-        }
-    }
-
-    newGame(event) {
-        console.log('reached it')
-        if(this.props.newGame) {
-            this.props.newGame();
-        }
-    }
-    
+export default class TopNav extends React.Component {    
     render() {
         return (
             <nav>
                 <ul className="clearfix">
                     <li>
-                        <a className="what" href="#" onClick={e => this.info(e)}>
+                        <a className="what" href="#" onClick={e => this.props.info(e)}>
                             What?
                         </a>
                     </li>
                     <li>
-                        <a className="new" href="#" onClick={e => this.newGame()}>
+                        <a className="new" href="#" onClick={e => this.props.newGame(e)}>
                             + New Game
                         </a>
                     </li>
